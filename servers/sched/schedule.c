86961f54f2a46b0454e54f63e29ec3d019ad3398
zhuchen <402298655@qq.com>
2017-12-01 09:34:09 +0800

    lottery:2.1

--- servers/sched/schedule.c
+++ servers/sched/schedule.c
@@ -11,6 +11,9 @@
 #include "schedproc.h"
 #include <assert.h>
 #include <minix/com.h>
+#include <minix/syslib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <machine/archtypes.h>
 #include "kernel/proc.h" /* for queue constants */
 
